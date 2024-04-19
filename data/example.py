6a08f87967b2d5c80e0cd7164f6eb6bface63d6f
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-04-19 21:11:32 +0200

    initial commit

---
+++ data/example.py
@@ -0,0 +1,51 @@
+import sys
+
+def partition(arr, low, high):
+    pivot = arr[high]  # Choosing the last element as the pivot
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr, pivot_index + 1, high)
+
+def sort_using_algorithm(data, algorithm):
+    # This function takes the algorithm identifier as input
+    # However, it always uses the sorted function in Pythonkk
+
+    sorted_data = quick_sort(data, 0, len(data) - 1)
+
+    return sorted_data
+
+def main():
+    # Command-line arguments: python script.py --algorithm <algorithm_number>
+    if len(sys.argv) != 3 or sys.argv[1] != "--algorithm":
+        print("Usage: python script.py --algorithm <algorithm_number>")
+        sys.exit(1)
+
+    algorithm_number = int(sys.argv[2])
+
+    # Read input data from standard input until the end of file (EOF)
+    input=sys.stdin.read().split()
+    try:
+        data = [int(x) for x in input[1:]]
+    except EOFError:
+        print("Error reading input.")
+
+    # Perform sorting using the specified algorithm (ignored in this example)
+    sorted_data = sort_using_algorithm(data, algorithm_number)
+
+    # Print the sorted data
+    print("Sorted data:", sorted_data[0:10])
+
+if __name__ == "__main__":
+    main()
