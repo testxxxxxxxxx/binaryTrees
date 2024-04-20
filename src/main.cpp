1ee01bd3b3443270b9db3eee9c6325729e3065e3
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-04-20 18:38:11 +0200

    created AVL tree creation algorithm and BST tree algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,151 @@
 
 using namespace std;
 
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+
+    Node(int value = 0): key(value), left(nullptr), right(nullptr)
+    {
+
+    }
+
+};
+
+Node* newNode(int key) {
+    Node* node = new Node();
+    node->key = key;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->height = 1;
+    return node;
+}
+
+int height(Node* node) {
+    if (node == nullptr)
+        return 0;
+    return node->height;
+}
+
+int getBalance(Node* node) {
+    if (node == nullptr)
+        return 0;
+    return height(node->left) - height(node->right);
+}
+
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    return x;
+}
+
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+Node* insertAVL(Node* node, int key) {
+
+    if (node == nullptr)
+        return newNode(key);
+
+    if (key < node->key)
+        node->left = insertAVL(node->left, key);
+    else if (key > node->key)
+        node->right = insertAVL(node->right, key);
+    else 
+        return node;
+
+    node->height = 1 + max(height(node->left), height(node->right));
+
+    int balance = getBalance(node);
+
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+Node* insertBST(Node* root, int key) {
+    if (root == nullptr)
+        return new Node(key);
+
+    if (key < root->key)
+        root->left = insertBST(root->left, key);
+    else if (key > root->key)
+        root->right = insertBST(root->right, key);
+
+    return root;
+}
+Node* constructBinarySearchTree(int arr[], int size) {
+    Node* root = nullptr;
+
+    for (int i = 0; i < size; i++) {
+        root = insertBST(root, arr[i]);
+    }
+
+    return root;
+}
+void inOrder(Node* root) {
+    if (root == nullptr)
+        return;
+    inOrder(root->left);
+    cout << root->key << " ";
+    inOrder(root->right);
+}
+
 int main(int argc, char *argv[])
 {
+    Node *root = nullptr;
+
+    root = insertAVL(root, 10);
+    root = insertAVL(root, 20);
+    root = insertAVL(root, 30);
+    root = insertAVL(root, 40);
+    root = insertAVL(root, 50);
+    root = insertAVL(root, 25);
+
+    inOrder(root);
+
+    delete root;
+
+    int numbers[] = {50, 30, 20, 40, 70, 60, 80};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    Node *bst = constructBinarySearchTree(numbers, size);
+
+    cout<<endl;
+
+    inOrder(bst);
 
     return 0;
 }
