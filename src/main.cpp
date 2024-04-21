44ae7e6fcd0ce54bb3f1de351179b02d8af2f107
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-04-21 22:36:05 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+int maxValue = 0; 
+int minValue = 0;
+
 struct Node {
     int key;
     Node* left;
@@ -116,16 +119,112 @@ Node* constructBinarySearchTree(int arr[], int size) {
 
     return root;
 }
+void findMax(Node *root)
+{
+    if(!root)
+        return;
+
+    if(root->key > maxValue)
+        maxValue = root->key;
+
+    findMax(root->left);
+    findMax(root->right);
+
+}
+void findMin(Node *root)
+{
+    if(!root)
+        return;
+
+    if(root->key < minValue)
+        minValue = root->key;
+
+    findMin(root->left);
+    findMin(root->right);
+
+}
 void inOrder(Node* root) {
     if (root == nullptr)
         return;
     inOrder(root->left);
-    cout << root->key << " ";
+    cout << root->key << ", ";
     inOrder(root->right);
+}
+void preOrder(Node* root) {
+    if (root == nullptr)
+        return;
+
+    cout << root->key << ", ";
+
+    preOrder(root->left);
+    preOrder(root->right);
+}
+void postOrder(Node* root) {
+    if (root == nullptr)
+        return;
+
+    postOrder(root->left);
+    postOrder(root->right);
+
+    cout << root->key << ", ";
+}
+
+void print(Node *root)
+{
+    cout<<"In-order: ";
+    inOrder(root);
+    cout<<endl;
+    cout<<"Post-order: ";
+    postOrder(root);
+    cout<<endl;
+    cout<<"Pre-order: ";
+    preOrder(root);
+    cout<<endl;
+
+}
+void findMinMax(Node *root)
+{
+    cout<<endl;
+
+    maxValue = 0;
+    minValue = root->key;
+
+    findMax(root);
+    findMin(root);
+
+    cout<<"min: "<<minValue<<endl;
+    cout<<"max: "<<maxValue<<endl;
+
+}
+string exportTree(Node *root)
+{
+    string l_str = "";
+    string r_str = "";
+
+    if(!root->left && !root->right)
+        return "node " + to_string(root->key);
+
+    if(root->left)
+        l_str = "child " + exportTree(root->left);
+    else
+        l_str = "child[missing]";
+    
+    if(root->right)
+        r_str = "child " + exportTree(root->right);
+    else
+        r_str = "child[missing]";
+
+    return " node " + to_string(root->key) + " \\" + l_str + "\\" + r_str + "\\";
+
+}
+void removeKeys(Node *root, int key)
+{
+
 }
 
 int main(int argc, char *argv[])
 {
+
     Node *root = nullptr;
 
     root = insertAVL(root, 10);
@@ -137,16 +236,35 @@ int main(int argc, char *argv[])
 
     inOrder(root);
 
+    findMinMax(root);
+
+    print(root);
+
+    cout<<exportTree(root)<<endl;
+
     delete root;
 
     int numbers[] = {50, 30, 20, 40, 70, 60, 80};
     int size = sizeof(numbers) / sizeof(numbers[0]);
 
-    Node *bst = constructBinarySearchTree(numbers, size);
+    /*Node *bst = constructBinarySearchTree(numbers, size);
 
     cout<<endl;
 
-    inOrder(bst);
+    inOrder(bst);*/
+
+    string action = "action";
+    string command = "";
+
+    /*while(true)
+    {
+        cout<<action<<">";
+        cin>>command;
+
+        if(command == "Exit")
+            return 0;
+
+    }*/
 
     return 0;
 }
